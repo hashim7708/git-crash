a1e6b8039b062805390ddd15597c27b93c881e30
hashim7708 <mh6811172@gmail.com>
2024-02-28 18:35:53 +0500

    added section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Abc from './Abc';
+import XYZ from './XYZ';
 
 function App() {
   return (
     <div className="App">
       <Abc />
+      <XYZ />
     </div>
   );
 }
---
+++ src/Xyz.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const XYZ = () => {
+  return (
+    <div>
+      Remote branch
+    </div>
+  )
+}
+
+export default XYZ
